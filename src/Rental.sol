eb48bcbe3d3e248a9ac6f8c0241cd0cf13eecb36
azeezabidoye <azeezabidoye@yahoo.com>
2025-02-18 17:17:55 +0100

    Contract Updated

--- src/Rental.sol
+++ src/Rental.sol
@@ -81,7 +81,7 @@ contract CarRental {
             renters[walletAddress].end,
             renters[walletAddress].start
         );
-        uint timespanToMimutes = timeDuration / 60;
+        uint timespanToMimutes = timeDuration / 6000;
         return timespanToMimutes;
     }
 
