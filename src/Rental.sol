5e61b1439a5bef8e6d2d6b35bc631b6b9e360454
azeezabidoye <azeezabidoye@yahoo.com>
2025-03-18 12:31:18 +0100

    Contract Updated

--- src/Rental.sol
+++ src/Rental.sol
@@ -1,88 +1,101 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.7;
+pragma solidity ^0.8.28;
 
-contract CarRental {
-    // Declare the owner of contract
+contract Rental {
     address owner;
 
-    // Constructor
     constructor() {
         owner = msg.sender;
     }
 
-    // Add yourself as a Renter
+    // Add a new Renter
     struct Renter {
         address payable walletAddress;
-        string firstname;
-        string lastname;
+        string firstName;
+        string lastName;
         bool canRent;
-        bool isActive;
+        bool active;
         uint balance;
-        uint due;
+        uint amountDue;
         uint start;
         uint end;
     }
 
-    // Mapping Wallet addresses to Renters
+    // Mapping wallet addresses to each Renter
     mapping(address => Renter) public renters;
 
-    // Function for adding a new Renter
+    // Function for adding a Renter to the list of "Renters"
     function addRenter(
         address payable walletAddress,
-        string memory firstname,
-        string memory lastname,
+        string memory firstName,
+        string memory lastName,
         bool canRent,
-        bool isActive,
+        bool active,
         uint balance,
-        uint due,
+        uint amountDue,
         uint start,
         uint end
     ) public {
         renters[walletAddress] = Renter(
-            walletAddress,
-            firstname,
-            lastname,
+            payable(walletAddress),
+            firstName,
+            lastName,
             canRent,
-            isActive,
+            active,
             balance,
-            due,
+            amountDue,
             start,
             end
         );
     }
 
-    // Check out car
+    // Checkout car ==> Rent a new car
     function checkOut(address payable walletAddress) public {
-        renters[walletAddress].isActive = true;
+        require(
+            renters[walletAddress].amountDue == 0,
+            "You have a pending balance!"
+        );
+        require(
+            renters[walletAddress].canRent == true,
+            "Can't rent a car at the moment!"
+        );
         renters[walletAddress].canRent = false;
+        renters[walletAddress].active = true;
         renters[walletAddress].start = block.timestamp;
     }
 
-    // Check in car
+    // Check-in car ==> Return rented car
     function checkIn(address payable walletAddress) public {
-        renters[walletAddress].isActive = false;
-        renters[walletAddress].canRent = true;
+        require(
+            renters[walletAddress].active == true,
+            "Kindly checkout the car!"
+        );
+        renters[walletAddress].active = false;
         renters[walletAddress].end = block.timestamp;
 
-        // Set Due Amount
-        setAmountDue(walletAddress);
+        // Set amount due
+        setDueAmount(walletAddress);
     }
 
-    // Function to calculate Timespan
-    function renterTimespan(uint end, uint start) internal pure returns (uint) {
+    // Calculate the Timespan
+    function renterTimespan(uint start, uint end) internal pure returns (uint) {
         return end - start;
     }
 
-    // Get total duration of rent time
+    // Get total duration of car use
     function getTotalDuration(
         address payable walletAddress
     ) public view returns (uint) {
-        uint timeDuration = renterTimespan(
-            renters[walletAddress].end,
-            renters[walletAddress].start
+        require(
+            renters[walletAddress].active == false,
+            "The car is currently checked out!"
         );
-        uint timespanToMimutes = timeDuration / 60;
-        return timespanToMimutes;
+        uint timespan = renterTimespan(
+            renters[walletAddress].start,
+            renters[walletAddress].end
+        );
+        uint timespanInMinutes = timespan / 60;
+        return timespanInMinutes;
     }
 
     // Get contract balance
@@ -91,23 +104,48 @@ contract CarRental {
     }
 
     // Get Renter's balance
-    function getRenterBalance(
+    function balanceOfRenter(
         address payable walletAddress
     ) public view returns (uint) {
         return renters[walletAddress].balance;
     }
 
-    // Set Due Amount
-    function setAmountDue(address payable walletAddress) internal {
+    // Set Due amount
+    function setDueAmount(address payable walletAddress) internal {
         uint timespanMinutes = getTotalDuration(walletAddress);
-        uint fiveMinuteIncrement = timespanMinutes / 5;
-        renters[walletAddress].due = fiveMinuteIncrement * 50000000000000000;
+        uint fiveMinutesIncrement = timespanMinutes / 5;
+        renters[walletAddress].amountDue =
+            fiveMinutesIncrement *
+            5000000000000000;
     }
 
-    // Reset Rental Option
+    // Reset renter's position after checking-in
     function canRentCar(
         address payable walletAddress
     ) public view returns (bool) {
         return renters[walletAddress].canRent;
+    }
+
+    // Deposit fund
+    function deposit(address walletAddress) public payable {
+        renters[walletAddress].balance += msg.value;
+    }
+
+    // Make Payment after check-in
+    function makePayment(address walletAddress) public payable {
+        require(
+            renters[walletAddress].amountDue > 0,
+            "You do not have anything due at this time!"
+        );
+        require(
+            renters[walletAddress].balance > 0,
+            "You do not have enough fund to cover payment. Please make a deposit!"
+        );
+        renters[walletAddress].balance -= msg.value;
+        renters[walletAddress].canRent = true;
+        renters[walletAddress].active = false;
+        renters[walletAddress].amountDue = 0;
+        renters[walletAddress].start = 0;
+        renters[walletAddress].end = 0;
     }
 }
